3e3b21e2e75ab7d666d5a52c89142b18458a836a
Hellen Fernanda Lunardi <hellenfernandalg@gmail.com>
2025-07-29 17:46:05 -0300

    Atualizações

--- process/pizza.php
+++ process/pizza.php
@@ -2,4 +2,27 @@
 
 include_once("conn.php");
 
+$method = $_SERVER["REQUEST_METHOD"];
+
+// RESGATE DOS DADOS, MONTAGEM DO PEDIDO
+if($method === "GET") {
+
+    $bordasQuery = $conn->query("SELECT * FROM bordas;");
+
+    $bordas = $bordasQuery->fetchAll();
+
+    $massasQuery = $conn->query("SELECT * FROM massas;");
+
+    $massas = $massasQuery->fetchAll();
+
+    $saboresQuery = $conn->query("SELECT * FROM sabores;");
+
+    $sabores = $saboresQuery->fetchAll();
+
+    
+// CRIAÇÃO DO PEDIDO    
+} else if ($method === "POST") {
+
+}
+
 ?>
